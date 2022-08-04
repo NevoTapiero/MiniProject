cda9286162385fc52625999b97fba7206873da7a
NevoTapiero <nevotap1234@gmail.com>
2022-08-04 09:03:49 +0300

    added

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import session as login_session
+import pyrebase 
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
+
+
+config = {
+  "apiKey": "AIzaSyBmh8Ep16oRIce7BeRl4htBKBKxvmGcmPI",
+  "authDomain": "mini-project-1d2a7.firebaseapp.com",
+  "projectId": "mini-project-1d2a7",
+  "storageBucket": "mini-project-1d2a7.appspot.com",
+  "messagingSenderId": "691958211021",
+  "appId": "1:691958211021:web:020066c15142e9828a6484",
+  "measurementId": "G-CNCP9MJZL3","databaseURL": "https://mini-project-1d2a7-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+@app.route('/')
+def HomePage():
+	return render_template("GYMbrosHomePage.html")
+
+@app.route('/signup', methods = ['GET','POST'])
+def SignUp():
+  if request.method == 'POST':
+    email = request.form['email']
+    password = request.form['password']
+    user = {"email": email, "password": password}
+    try:
+      login_session['user'] = auth.create_user_with_email_and_password(email, password)
+      db.child('Users').child(login_session['user']['localId']).set(user)
+      return redirect(url_for("SignIn"))
+    except: 
+      error = "Authentication failed"
+  return render_template("signup.html")
+
+@app.route('/signin', methods = ['GET', 'POST'])
+def SignIn():
+  if request.method == 'POST':
+    email = request.form['email']
+    password = request.form['password']
+    try:
+      login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+      return redirect(url_for('HomePage'))
+    except:
+      error = "Authentication failed"
+  return render_template("signin.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
